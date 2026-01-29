a1d9f9944b849ddf1ca1eeb143fa8878bc0977fd
Tim Graves <gravest@users.noreply.github.com>
2026-01-29 13:12:06 -0800

    Adjust required validation for UAR requests

--- UAR.Web/Models/UarRequest.cs
+++ UAR.Web/Models/UarRequest.cs
@@ -6,11 +6,11 @@ public class UarRequest
 {
     public int Id { get; set; }
 
-    public string RequestNumber { get; set; } = string.Empty;
+    public string? RequestNumber { get; set; }
 
     public DateTime SubmittedOn { get; set; }
 
-    public string EmployeeNameChange { get; set; } = string.Empty;
+    public string? EmployeeNameChange { get; set; }
 
     [Required]
     public string Company { get; set; } = string.Empty;
@@ -27,7 +27,7 @@ public class UarRequest
     [Required]
     public string JobTitle { get; set; } = string.Empty;
 
-    public string DesiredEffectiveDate { get; set; } = string.Empty;
+    public string? DesiredEffectiveDate { get; set; }
 
     [Required]
     public string RequestorPhoneNumber { get; set; } = string.Empty;
@@ -35,30 +35,30 @@ public class UarRequest
     [Required]
     public string EmployeeStatus { get; set; } = string.Empty;
 
-    public string EmployeeType { get; set; } = string.Empty;
+    public string? EmployeeType { get; set; }
 
-    public string TransferFromProgram { get; set; } = string.Empty;
+    public string? TransferFromProgram { get; set; }
 
-    public string LeaveOfAbsence { get; set; } = string.Empty;
+    public string? LeaveOfAbsence { get; set; }
 
-    public string RegionName { get; set; } = string.Empty;
+    public string? RegionName { get; set; }
 
     [Required]
     public string Program1 { get; set; } = string.Empty;
 
-    public string Program2 { get; set; } = string.Empty;
+    public string? Program2 { get; set; }
 
-    public string Program3 { get; set; } = string.Empty;
+    public string? Program3 { get; set; }
 
-    public string OtherPrograms { get; set; } = string.Empty;
+    public string? OtherPrograms { get; set; }
 
-    public string EmployeeDeviceTypes { get; set; } = string.Empty;
+    public string? EmployeeDeviceTypes { get; set; }
 
-    public string OtherDeviceType { get; set; } = string.Empty;
+    public string? OtherDeviceType { get; set; }
 
-    public string HrRepresentativeName { get; set; } = string.Empty;
+    public string? HrRepresentativeName { get; set; }
 
-    public string SubmitForApproval { get; set; } = string.Empty;
+    public string? SubmitForApproval { get; set; }
 
     [Required]
     public string Status { get; set; } = string.Empty;
@@ -69,89 +69,89 @@ public class UarRequest
     [Required]
     public string ProgramAdministrator { get; set; } = string.Empty;
 
-    public string RdoApprover { get; set; } = string.Empty;
+    public string? RdoApprover { get; set; }
 
-    public string RejectionReason { get; set; } = string.Empty;
+    public string? RejectionReason { get; set; }
 
-    public string EmailAccount { get; set; } = string.Empty;
+    public string? EmailAccount { get; set; }
 
-    public string AccountEnableDisable { get; set; } = string.Empty;
+    public string? AccountEnableDisable { get; set; }
 
-    public string ForwardingEmailAddress { get; set; } = string.Empty;
+    public string? ForwardingEmailAddress { get; set; }
 
-    public string AssignedAccountUsername { get; set; } = string.Empty;
+    public string? AssignedAccountUsername { get; set; }
 
-    public string Office365License { get; set; } = string.Empty;
+    public string? Office365License { get; set; }
 
-    public string AdditionalMicrosoftProducts { get; set; } = string.Empty;
+    public string? AdditionalMicrosoftProducts { get; set; }
 
-    public string CreateEmailGroups { get; set; } = string.Empty;
+    public string? CreateEmailGroups { get; set; }
 
-    public string SharePointSiteUrl { get; set; } = string.Empty;
+    public string? SharePointSiteUrl { get; set; }
 
-    public string SharePointSiteDescription { get; set; } = string.Empty;
+    public string? SharePointSiteDescription { get; set; }
 
-    public string ExistingEmailGroups { get; set; } = string.Empty;
+    public string? ExistingEmailGroups { get; set; }
 
-    public string AdditionalSharepointAccess { get; set; } = string.Empty;
+    public string? AdditionalSharepointAccess { get; set; }
 
-    public string NetworkSharedDrives { get; set; } = string.Empty;
+    public string? NetworkSharedDrives { get; set; }
 
-    public string TopAccess { get; set; } = string.Empty;
+    public string? TopAccess { get; set; }
 
-    public string KronosAccessTypes { get; set; } = string.Empty;
+    public string? KronosAccessTypes { get; set; }
 
-    public string KronosAccessDetails { get; set; } = string.Empty;
+    public string? KronosAccessDetails { get; set; }
 
-    public string LawsonAccess { get; set; } = string.Empty;
+    public string? LawsonAccess { get; set; }
 
-    public string AdditionalLawsonAccess { get; set; } = string.Empty;
+    public string? AdditionalLawsonAccess { get; set; }
 
-    public string MileageAccess { get; set; } = string.Empty;
+    public string? MileageAccess { get; set; }
 
-    public string ComericaBankingAccess { get; set; } = string.Empty;
+    public string? ComericaBankingAccess { get; set; }
 
-    public string TrustRepPayeeAccess { get; set; } = string.Empty;
+    public string? TrustRepPayeeAccess { get; set; }
 
-    public string QuickbooksAccess { get; set; } = string.Empty;
+    public string? QuickbooksAccess { get; set; }
 
-    public string CoWorkerAccess { get; set; } = string.Empty;
+    public string? CoWorkerAccess { get; set; }
 
-    public string AdditionalHrFinanceAccess { get; set; } = string.Empty;
+    public string? AdditionalHrFinanceAccess { get; set; }
 
-    public string OrderConnectAccess { get; set; } = string.Empty;
+    public string? OrderConnectAccess { get; set; }
 
-    public string CaminarAccess { get; set; } = string.Empty;
+    public string? CaminarAccess { get; set; }
 
-    public string AvatarAccess { get; set; } = string.Empty;
+    public string? AvatarAccess { get; set; }
 
-    public string AdverseEventSupervisorAccess { get; set; } = string.Empty;
+    public string? AdverseEventSupervisorAccess { get; set; }
 
-    public string AdverseEventAdditionalProgramAccess { get; set; } = string.Empty;
+    public string? AdverseEventAdditionalProgramAccess { get; set; }
 
-    public string BusinessIntelligenceRole { get; set; } = string.Empty;
+    public string? BusinessIntelligenceRole { get; set; }
 
-    public string AdverseEventNonEmployeeAccess { get; set; } = string.Empty;
+    public string? AdverseEventNonEmployeeAccess { get; set; }
 
-    public string TelehealthAccess { get; set; } = string.Empty;
+    public string? TelehealthAccess { get; set; }
 
-    public string NextTelehealthSession { get; set; } = string.Empty;
+    public string? NextTelehealthSession { get; set; }
 
-    public string AdditionalComments { get; set; } = string.Empty;
+    public string? AdditionalComments { get; set; }
 
-    public string PharmericaAccess { get; set; } = string.Empty;
+    public string? PharmericaAccess { get; set; }
 
-    public string PharmericaUserRole { get; set; } = string.Empty;
+    public string? PharmericaUserRole { get; set; }
 
-    public string PharmericaUsername { get; set; } = string.Empty;
+    public string? PharmericaUsername { get; set; }
 
-    public string CallCenterAccess { get; set; } = string.Empty;
+    public string? CallCenterAccess { get; set; }
 
-    public string AdobeSignAccount { get; set; } = string.Empty;
+    public string? AdobeSignAccount { get; set; }
 
-    public string CopilotLicense { get; set; } = string.Empty;
+    public string? CopilotLicense { get; set; }
 
-    public string SmartsheetLicense { get; set; } = string.Empty;
+    public string? SmartsheetLicense { get; set; }
 
-    public string EFax { get; set; } = string.Empty;
+    public string? EFax { get; set; }
 }
